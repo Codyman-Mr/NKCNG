8346abc5ffe923be4b7d43275ea27f399fa80246
Codyman-Mr <willbrightuyole@gmail.com>
2025-11-16 06:50:40 -0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,14 @@ set -e
 
 echo "Starting Laravel container..."
 
-# Run Laravel migrations runtime
+# Run Laravel migrations
+echo "Running Laravel migrations..."
 php artisan migrate --force || echo "Migration failed, continuing..."
 
 # Clear & cache config
 php artisan config:clear
 php artisan config:cache
 
-# Start Supervisor for nginx + php-fpm + workers
+# Start Supervisor (long-running processes)
+echo "Starting Supervisor..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
