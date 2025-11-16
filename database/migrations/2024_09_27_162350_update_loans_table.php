583cb7fc5ac3858ffd2fbaef4414c603be803549
Codyman-Mr <willbrightuyole@gmail.com>
2025-11-16 09:40:48 -0800

    Update 2024_09_27_162350_update_loans_table.php

--- database/migrations/2024_09_27_162350_update_loans_table.php
+++ database/migrations/2024_09_27_162350_update_loans_table.php
@@ -9,20 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('loans', function (Blueprint $table) {
-            $table->string('applicant_name')->nullable()->after('id');
-        });
-    }
+   public function up(): void
+{
+    Schema::table('loans', function (Blueprint $table) {
+        $table->string('applicant_name')->nullable()->after('loan_required_amount');
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('loans', function (Blueprint $table) {
-            $table->dropColumn('applicant_name');
-        });
-    }
-};
+public function down(): void
+{
+    Schema::table('loans', function (Blueprint $table) {
+        $table->dropColumn('applicant_name');
+    });
+}
